fa2f60809a9f1e3b4d12896771c40f285e186bce
elygiux <eligijuslipskis@gmail.com>
2015-08-15 15:06:36 +0300

    Telling back to create game room and listening for created game room.

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,15 @@ $.connection.hub.url = "http://172.16.243.156:18080/signalr";
 
 var con = $.hubConnection();
 var hub = $.connection.mainTowerDefenseHub;
-hub.on('onMonitorConnected', function (game) {
-
-});
 
 $.connection.hub.start()
     .done(function () {
-        hub.server.connectMonitor();
+        hub.server.createGameRoom();
     });
+
+hub.on('gameRoomCreated', function () {
+
+});
 
 
 var Game = (function () {
