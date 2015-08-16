6e3246998b49585277971bd218f7e58e8cf21813
shadowlight12 <augustinaff@gmail.com>
2015-08-16 09:25:25 +0300

    base added

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,40 @@
+var targetList = [];
+var projector, mouse = { x: 0, y: 0 };
+
+function onDocumentMouseDown( event ) 
+{
+    // the following line would stop any other event handler from firing
+    // (such as the mouse's TrackballControls)
+    // event.preventDefault();
+    
+    console.log("Click.");
+    
+    // update the mouse variable
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    // find intersections
+
+    // create a Ray with origin at the mouse position
+    //   and direction into the scene (camera direction)
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+    projector.unprojectVector( vector, Game.camera );
+    var ray = new THREE.Raycaster( Game.camera.position, vector.sub( Game.camera.position ).normalize() );
+
+    // create an array containing all objects in the scene with which the ray intersects
+    var intersects = ray.intersectObjects( targetList );
+    
+    // if there is one (or more) intersections
+    if ( intersects.length > 0 )
+    {
+        console.log("Hit @ " + toString( intersects[0].point ) );
+        // change the color of the closest face.
+        intersects[ 0 ].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 ); 
+        intersects[ 0 ].object.geometry.colorsNeedUpdate = true;
+    }
+
+}
+
 var Game = (function () {
     'use strict';
 
@@ -7,6 +44,7 @@ var Game = (function () {
     var renderer = new THREE.WebGLRenderer();
     var bullet = Bullet;
     var attacker = Attacker;
+    projector = new THREE.Projector();
 
     function resetGame(attacker, bullet) {
 
@@ -21,8 +59,9 @@ var Game = (function () {
     function init() {
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setClearColor(0x000000);
-        pointerLock.init(camera, scene);
+        //pointerLock.init(camera, scene);
         document.body.appendChild(renderer.domElement);
+        document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     }
 
     function render() {
@@ -64,12 +103,12 @@ var Game = (function () {
     };
 })();
 
-
 $(document).ready(function () {
     $.connection.hub.url = "http://192.168.1.107:43210/signalr";
     var game = Game;
     var map = Map;
     var tower = Tower;
+    var base = Base;
     var bullet = Bullet;
     var con = $.hubConnection();
     var hub = $.connection.myHub;
@@ -78,6 +117,7 @@ $(document).ready(function () {
     var posY_array = {};
     var pos_cell_id = {};
     var towers = {};
+    var bases = {};
 
     game.init();
 
@@ -129,8 +169,9 @@ $(document).ready(function () {
                 var cellId = data.Cells[index].CellId;
                 var posX = data.Cells[index].PosX;
                 var posY = data.Cells[index].PosY;
-                towers[i] = tower.createTower(game, posY * 17 - 230, 40, (posX * -10) + (game.mapSize/2), cellId);
-                hub.server.placeTower(cellId);
+                bases[i] = base.createBase(game, posY * 17 - 230, 40, (posX * -10) + (game.mapSize/2), cellId);
+                //hub.server.placeTower(cellId);
+                targetList.push(bases[i]);
                 i++;                
             }
         });
---
+++ src/js/base.js
@@ -0,0 +1,25 @@
+var Base = (function () {
+    'use strict';
+
+    var bases = [];
+
+    function createBase(game, posX, posY, posZ, id) {
+        
+        var base = new Physijs.BoxMesh(new THREE.CubeGeometry(40, 10, 40), new THREE.MeshLambertMaterial({color: 0x4CAF50}), 0);
+        
+        base.name = "base";
+        base.position.x = posX;
+        base.position.y = posY;
+        base.position.z = posZ;
+        base.towerId = id;
+        game.scene.add(base);
+        
+        bases.push(base);
+        return base;
+    }
+
+    return {
+        createBase: createBase,
+        base: bases
+    };
+})();
